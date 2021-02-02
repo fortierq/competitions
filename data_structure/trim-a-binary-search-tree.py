b0781a5f70001b914c59563a8d30a9e3a7e0c2db
Quentin Fortier <qpfortier@gmail.com>
2021-02-02 20:52:06 +0100

    Add trim-a-binary-search-tree

---
+++ data_structure/trim-a-binary-search-tree.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/trim-a-binary-search-tree/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def trimBST(self, root: TreeNode, low: int, high: int) -> TreeNode:
+        if root is None:
+            return None
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        if root.val > high:
+            return self.trimBST(root.left, low, high)
+        return TreeNode(root.val, self.trimBST(root.left, low, high), self.trimBST(root.right, low, high))
