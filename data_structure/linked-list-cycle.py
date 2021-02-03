aeafa6c8c101cc7301d49f5357a38dd9d1bb8a2a
Quentin Fortier <qpfortier@gmail.com>
2021-02-03 19:54:23 +0100

    Remove recursivity in  linked-list-cycle with

--- data_structure/linked-list-cycle.py
+++ data_structure/linked-list-cycle.py
@@ -7,16 +7,19 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        def next(tortue, lapin):
-            if lapin is None or lapin.next is None:
-                return False
-            tortue = tortue.next
-            lapin = lapin.next.next
-            return lapin is tortue or next(tortue, lapin)
-        return next(head, head)
+        try:
+            tortue = head
+            lapin = head.next
+            while lapin is not tortue:
+                tortue = tortue.next
+                lapin = lapin.next.next
+            return True
+        except AttributeError:
+            return False
 
 s = Solution()
 list = ListNode(0)
+assert not s.hasCycle(list)
 list.next = ListNode(1)
 list.next.next = ListNode(2)
 assert not s.hasCycle(list)
