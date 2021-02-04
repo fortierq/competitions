def245c34143f5330548e725f130ce1a5baed6bd
Quentin Fortier <qpfortier@gmail.com>
2021-02-04 19:53:40 +0100

    Update lhs

--- other/longest-harmonious-subsequence.py
+++ other/longest-harmonious-subsequence.py
@@ -2,17 +2,18 @@
 
 class Solution:
     def findLHS(self, nums) -> int:
-        lhs_maxi = {}  # lhs_maxi[v] is the LHS whose maximum is v
+        count = {}
         for e in nums:
-            for m in [e - 1, e]:
-                if m not in lhs_maxi:
-                    lhs_maxi[m] = 0
-                lhs_maxi[m] += 1
-        val = lhs_maxi.values()
-        if len(val) == 0:
-            return 0
-        return max(val)
+            if e not in count:
+                count[e] = 0
+            count[e] += 1
+        maxi = 0
+        for k in count:
+            if k - 1 in count:
+                maxi = max(maxi, count[k] + count[k - 1])
+        return maxi
 
 s = Solution()
 assert s.findLHS([1,3,2,2,5,2,3,7]) == 5
 assert s.findLHS([]) == 0
+assert s.findLHS([1, 1, 1]) == 0
