db6d10426be168d65ac2a5b7792cd22dcf410149
Quentin Fortier <qpfortier@gmail.com>
2021-02-11 18:36:24 +0100

    Use Counter for anagram

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -1,9 +1,7 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        ds, dt = dict(), dict()
-        for u, d in (s, ds), (t, dt):
-            for c in u:
-                if c not in d:
-                    d[c] = -1 
-                d[c] += 1
-        return ds == dt
+        return Counter(s) == Counter(t)
+
+S = Solution()
+assert S.isAnagram("test", "stet")
