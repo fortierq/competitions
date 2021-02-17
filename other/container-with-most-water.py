9b05f2db66faedf84c7a7ac60e75ee0409267131
Quentin Fortier <qpfortier@gmail.com>
2021-02-17 19:17:59 +0100

    Simplify container-with-most-water solution

--- other/container-with-most-water.py
+++ other/container-with-most-water.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxArea(self, height) -> int:
-        def area(i, j): 
+        def area(i, j):
             return (j - i) * min(height[i], height[j])
 
         i, j = 0, len(height) - 1
@@ -8,13 +8,7 @@ class Solution:
         while i < j:
             m = max(m, area(i, j))
             if height[i] < height[j]:
-                ii = i + 1
-                while ii < j and height[ii] <= height[i]:
-                    ii += 1
-                i = ii
+                i += 1
             else:
-                jj = j - 1
-                while jj > i and height[jj] <= height[j]:
-                    jj -= 1
-                j = jj
+                j -= 1
         return m
