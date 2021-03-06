576de03e2201ccd968aefa391df88737ea0328b4
Quentin Fortier <qpfortier@gmail.com>
2021-03-06 15:07:25 +0100

    Solve with trie and dict

--- string/short-encoding-of-words.py
+++ string/short-encoding-of-words.py
@@ -1,14 +1,16 @@
+
 class Solution:
     def minimumLengthEncoding(self, words) -> int:
-        keep = []
-        for w1 in words:
-            add = True
-            for i, w2 in enumerate(keep):
-                if w1.endswith(w2):
-                    add = False
-                    keep[i] = w1
-                if w2.endswith(w1):
-                    add = False
-            if add:
-                keep.append(w1)
-        return sum(len(w) for w in keep)
+        trie = {}
+        for w in words:
+            node = trie
+            for c in w[::-1]:
+                if c not in node:
+                    node[c] = {}
+                node = node[c]
+            
+        def dfs(node, depth):
+            if not node:
+                return depth + 1
+            return sum(dfs(node[v], depth + 1) for v in node)
+        return dfs(trie, 0)
