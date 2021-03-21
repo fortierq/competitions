685550d5c62841da41c91bfe0964d497e3e0b153
Quentin Fortier <qpfortier@gmail.com>
2021-03-21 11:56:06 +0100

    Use Counter

--- algebra/reordered-power-of-2.py
+++ algebra/reordered-power-of-2.py
@@ -1,14 +1,14 @@
 import numpy as np
-
+from collections import Counter
 
 class Solution:
     def reorderedPowerOf2(self, N: int) -> bool:
         def sorted_digits(n):
             return sorted(map(int, list(str(n))))
-        d = sorted_digits(N)
-        p = len(d)
+        d = Counter(str(N))
+        p = sum(d.values())
         l = np.log2(10)
         for k in range(int((p-1)*l), int(p*l) + 1):
-            if d == sorted_digits(1 << k):
+            if d == Counter(str(1 << k)):
                 return True
         return False
