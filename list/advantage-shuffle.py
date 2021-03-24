af1ccf6e5e59d6d8498b1b185ed91c6ee44caca7
Quentin Fortier <qpfortier@gmail.com>
2021-03-24 19:32:53 +0100

    Use deque and sorting

--- list/advantage-shuffle.py
+++ list/advantage-shuffle.py
@@ -1,9 +1,11 @@
+from collections import deque
+
 class Solution:
     def advantageCount(self, A: List[int], B: List[int]) -> List[int]:
         order = [i for i in range(len(B))]
         ans = [0 for _ in range(len(A))]
         order.sort(key=lambda x: -B[x])
-        A.sort()
+        A = deque(sorted(A))
         for ix in order:
-            ans[ix] = A.pop() if A[-1] > B[ix] else A.pop(0)
+            ans[ix] = A.pop() if A[-1] > B[ix] else A.popleft()
         return ans
