8d7c72a867a431651bb2a79c2ae6220520bfb489
Quentin Fortier <qpfortier@gmail.com>
2021-03-24 18:36:28 +0100

    Solve by sorting

--- list/advantage-shuffle.py
+++ list/advantage-shuffle.py
@@ -1,15 +1,9 @@
 class Solution:
     def advantageCount(self, A: List[int], B: List[int]) -> List[int]:
-        res = []
-        for b in B:
-            j, v = -1, float("inf")
-            for i, a in enumerate(A):
-                if a == -1:
-                    continue
-                if b < a < v:
-                    j, v = i, a
-            if j == -1:
-                
-            res.append(A[j])
-            A[j] = -1
-        return res
+        order = [i for i in range(len(B))]
+        ans = [0 for _ in range(len(A))]
+        order.sort(key=lambda x: -B[x])
+        A.sort()
+        for ix in order:
+            ans[ix] = A.pop() if A[-1] > B[ix] else A.pop(0)
+        return ans
