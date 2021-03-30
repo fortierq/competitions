33d8316cc0b91fd2c73089bf1959e83963b2fc4c
Quentin Fortier <qpfortier@gmail.com>
2021-03-30 22:04:07 +0200

    Sort + DP (WIP)

--- list/russian-doll-envelopes.py
+++ list/russian-doll-envelopes.py
@@ -1,21 +1,6 @@
-
 class Solution:
-    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        n = len(envelopes)
-        G = {v : [] for v in range(n)}
-        cur = set(v for v in range(n))
-        for i, e in enumerate(envelopes):
-            for j, f in enumerate(envelopes):
-                if e[0] < f[0] and e[1] < f[1]:
-                    G[i].append(j)
-                    cur.discard(j)
-        d = 0
-        next = set()
-        while len(cur) != 0:
-            u = cur.pop()
-            for v in G[u]:
-                next.add(v)
-            if len(cur) == 0:
-                d += 1
-                cur, next = next, set()
-        return d
+    def maxEnvelopes(self, envelopes):
+        nums = sorted(envelopes, key = lambda x: [x[0], -x[1]])    
+        dp = [10**10] * (len(nums) + 1)
+        for elem in nums: dp[bisect_left(dp, elem[1])] = elem[1]  
+        return dp.index(10**10)
