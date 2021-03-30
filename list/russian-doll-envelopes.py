d2309ffbb09e27667df1e49c5141e381ed03e9d6
Quentin Fortier <qpfortier@gmail.com>
2021-03-30 21:47:44 +0200

    TLE with graph traversal

--- list/russian-doll-envelopes.py
+++ list/russian-doll-envelopes.py
@@ -1,12 +1,21 @@
+
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
-        L = []
+        n = len(envelopes)
+        G = {v : [] for v in range(n)}
+        cur = set(v for v in range(n))
         for i, e in enumerate(envelopes):
-            m = 1
-            for j in range(i):
-                m_ = L[j]
-                if e[0] >= envelopes[j][0] and e[1] >= envelopes[j][1]:
-                    m_ += 1
-                m = max(m, m_)
-            L.append(m)
-        return max(L)
+            for j, f in enumerate(envelopes):
+                if e[0] < f[0] and e[1] < f[1]:
+                    G[i].append(j)
+                    cur.discard(j)
+        d = 0
+        next = set()
+        while len(cur) != 0:
+            u = cur.pop()
+            for v in G[u]:
+                next.add(v)
+            if len(cur) == 0:
+                d += 1
+                cur, next = next, set()
+        return d
