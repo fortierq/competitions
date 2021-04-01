b886debbd35de7a5a37bef3d9f327a28c57bd813
Quentin Fortier <qpfortier@gmail.com>
2021-04-01 18:52:25 +0200

    SOlution by splitting list

--- list/palindrome-linked-list.py
+++ list/palindrome-linked-list.py
@@ -1,13 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def isPalindrome(self, head: ListNode) -> bool:
-        cur = head
-        L = []
-        while cur:
-            L.append(cur.val)
-            cur = cur.next
-        return L == L[::-1]
+def isPalindrome(self, head):
+    fast = slow = head
+    # find the mid node
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+    # reverse the second half
+    node = None
+    while slow:
+        nxt = slow.next
+        slow.next = node
+        node = slow
+        slow = nxt
+    # compare the first and second half nodes
+    while node: # while node and head:
+        if node.val != head.val:
+            return False
+        node = node.next
+        head = head.next
+    return True
