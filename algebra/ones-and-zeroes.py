19702b219a607b34753591bde1174ccc902c7dfb
Quentin Fortier <qpfortier@gmail.com>
2021-04-02 18:35:38 +0200

    Use DP

---
+++ algebra/ones-and-zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxForm(self, strs, m, n):
+        xy = [[s.count("0"), s.count("1")] for s in strs]
+
+        @cache
+        def dp(mm, nn, kk):
+            if mm < 0 or nn < 0: return -float("inf")
+            if kk == len(strs): return 0
+            x, y = xy[kk]
+            return max(1 + dp(mm-x, nn-y, kk + 1), dp(mm, nn, kk + 1))
+        
+        return dp(m, n, 0)
