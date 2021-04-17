40f19a8c2261de514b9f0681f2ce2d45ae5905ed
Quentin Fortier <qpfortier@gmail.com>
2021-04-17 16:23:13 +0200

    Use np, cumsum and subarraySum

---
+++ array/number-of-submatrices-that-sum-to-target.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def subarraySum(nums, k: int) -> int:
+    d = Counter()
+    d[0] = 1
+    res, cum_sum = 0, 0
+    for n in nums:
+        cum_sum += n
+        res += d[cum_sum - k]
+        d[cum_sum] += 1
+    return res
+
+class Solution:
+    def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
+        M = np.array(matrix).cumsum(axis=1)
+        res = 0
+        for j in range(len(M[0])):
+            for i in range(j + 1):
+                a = M[:, j] - (M[:, i - 1] if i > 0 else 0)
+                res += subarraySum(a.T, target)
+        return res
