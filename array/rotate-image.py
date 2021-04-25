b19a8fd2e5c879cad933d1ef13bd478e3d66b80b
Quentin Fortier <qpfortier@gmail.com>
2021-04-25 11:18:00 +0200

    Use rotation

--- array/rotate-image.py
+++ array/rotate-image.py
@@ -1,16 +1,18 @@
-import numpy as np
-
 
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
+    def rotate(self, m: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
-        """
-        n = len(matrix)
-        m = np.ones((n, n), int)
-        for i in range(n):
-            for j in range(n):
-                m[j, n - 1 - i] = matrix[i][j]
-        for i in range(n):
-            for j in range(n):
-                matrix[i][j] = m[i, j]
+        """            
+        n = len(m)
+        def rot(i, j):
+            return n - 1 - j, i
+        
+        for i in range((n+1)//2):
+            for j in range(n//2):
+                k, l = i, j
+                tmp = m[i][j]
+                for p in range(4):
+                    k_, l_ = rot(k, l)
+                    m[k][l] = m[k_][l_] if p < 3 else tmp
+                    k, l = k_, l_
