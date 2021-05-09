3ed1003b6a7ff0be17f46920f4a5f1bf279bffcd
Quentin Fortier <qpfortier@gmail.com>
2021-05-09 14:30:24 +0200

    Update construct-target-array-with-multiple-sums.py

--- other/construct-target-array-with-multiple-sums.py
+++ other/construct-target-array-with-multiple-sums.py
@@ -4,11 +4,10 @@ class Solution:
     def isPossible(self, target: List[int]) -> bool:
         q = [-t for t in target]
         heapify(q)  # use max heap
-        while True:
+        while q[0] != 1:
             maxi = -heappop(q)
             s = -sum(q)
-            if maxi == 1:  # there are only ones
-                return True
             if maxi <= s or s == 0:  # this is not possible
                 return False
             heappush(q, -(maxi % s))  # remove s from maxi while possible
+        return True
