fa77f138b65be9dd922daa55dc7778aa7f8a5fe9
Quentin Fortier <qpfortier@gmail.com>
2021-06-11 20:52:49 +0200

    Use dp and partial sum

--- array/stone-game-vii.rs
+++ array/stone-game-vii.rs
@@ -3,20 +3,14 @@ use std::cmp;
 impl Solution {
     pub fn stone_game_vii(stones: Vec<i32>) -> i32 {
         let n = stones.len();
-        let mut dp = vec![vec![0; n]; n]; // dp[i][i+k] is 
-        let mut s = vec![vec![0; n]; n];
-        for i in 0..n {
-            s[i][i] = stones[i];
+        let mut dp = vec![vec![0; n]; n]; // dp[i][j] will be the maximum gain for a player using stones[i..j]
+        let mut s = vec![0; n+1];  // s[i] will be the sum of stones[..i]
+        for i in 1..n+1 {
+            s[i] = s[i-1] + stones[i-1];
         }
         for k in 1..n {
             for i in 0..n - k {
-                s[i][i+k] = s[i][i+k-1] + stones[i+k];
-                if (n - 1 - k) % 2 == 0 {
-                    dp[i][i+k] = cmp::max(dp[i][i+k-1] + s[i][i+k-1], dp[i+1][i+k] + s[i+1][i+k]);
-                }
-                else {
-                    dp[i][i+k] = cmp::min(dp[i][i+k-1] - s[i][i+k-1], dp[i+1][i+k] - s[i+1][i+k]);
-                }
+                dp[i][i+k] = cmp::max(-dp[i][i+k-1] + (s[i+k] - s[i]), -dp[i+1][i+k] + (s[i+k+1] - s[i+1]));
             }
         }
         return dp[0][n-1];
