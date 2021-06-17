0680e9cf7b6726131374f1d0a88d44dce5529da0
Quentin Fortier <qpfortier@gmail.com>
2021-06-17 21:22:02 +0200

    Use dp

--- list/number-of-subarrays-with-bounded-maximum.rs
+++ list/number-of-subarrays-with-bounded-maximum.rs
@@ -1,24 +1,18 @@
-fn n_subarray(i: i32, j: i32) -> i32 {
-    let sz = j - 1 - i;
-    return sz*(sz + 1)/2;
-}
-
 impl Solution {
     pub fn num_subarray_bounded_max(nums: Vec<i32>, left: i32, right: i32) -> i32 {
         let mut k = 0;
         let mut last_big = -1;
-        let mut valid = 0;
         let mut total = 0;
         for (i, &n) in nums.iter().enumerate() {
             if n > right {
-                total += valid * n_subarray(last_big, i as i32);
-                valid = 0;
+                k = 0;
                 last_big = i as i32;
             }
             else if n >= left {
-                valid = 1;
+                k = i as i32 - last_big;
             }
+            total += k;
         }
-        return total + valid*n_subarray(last_big, nums.len() as i32);
+        return total;
     }
 }
