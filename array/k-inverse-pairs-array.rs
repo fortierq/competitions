53e4503c6b4f39484f114d890ea080f8d90b32d0
Quentin Fortier <qpfortier@gmail.com>
2021-06-19 13:57:27 +0200

    Improve dp solution

--- array/k-inverse-pairs-array.rs
+++ array/k-inverse-pairs-array.rs
@@ -2,19 +2,19 @@ impl Solution {
     pub fn k_inverse_pairs(n: i32, k: i32) -> i32 {
         let n = n as usize;
         let k = k as usize;
-        let mut inv : Vec<i32> = vec![0; (n+1)*(k+1)];
+        let mut inv = vec![vec![0; k + 1]; n + 1];
         for i in 0..=n {
-            inv[i*(k+1)] = 1;
+            inv[i][0] = 1;
         }
         for i in 1..=n {
             for j in 1..=k {
                 for l in 0..=j {
                     if l < i {
-                        inv[i*(k+1) + j] = (inv[i*(k+1) + j] + inv[(i - 1)*(k+1) + j - l]) % 1000000007;
+                        inv[i][j] = (inv[i][j] + inv[i - 1][j - l]) % 1000000007;
                     }
                 }
             }
         }
-        inv[n*(k+1) + k]
+        inv[n][k]
     }
 }
