eecfd9c57841445efd5b55d39136520fb8108728
Quentin Fortier <qpfortier@gmail.com>
2021-06-19 21:00:57 +0200

    Delete all-binary-codes-of-size-k.py

--- algebra/all-binary-codes-of-size-k.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasAllCodes(self, s: str, k: int) -> bool:
-        seen = set()
-        for i in range(len(s) - k + 1):
-            m = s[i:i+k]
-            if m not in seen:
-                seen.add(m)
-        return len(seen) == 2**k
- 
