16e611412d8028dfccd8ef4fa1fdb67c551d26f7
Quentin Fortier <qpfortier@gmail.com>
2021-06-19 18:25:51 +0200

    Solution with dp (prev: heap)

--- list/minimum-number-of-refueling-stops.rs
+++ list/minimum-number-of-refueling-stops.rs
@@ -1,22 +1,18 @@
-use std::collections::BinaryHeap;
-
 impl Solution {
-    pub fn min_refuel_stops(target: i32, mut fuel: i32, mut stations: Vec<Vec<i32>>) -> i32 {
-        stations.push(vec![target, 0]);
-        let mut heap : BinaryHeap<i32> = BinaryHeap::new(); // unused stations seen so far
-        let mut k = 0; // number of refuels so far
-        for s in stations.iter() {
-            while fuel < s[0] {
-                match heap.pop() {
-                    Some(f) => {
-                        fuel = fuel + f; 
-                        k = k + 1
-                    },
-                    None => return -1
+    pub fn min_refuel_stops(target: i32, start_fuel: i32, stations: Vec<Vec<i32>>) -> i32 {
+        let mut dp = vec![start_fuel; stations.len() + 1]; // dp[j] is the max distance with at most j refuels
+        for (i, s) in stations.iter().enumerate() {
+            for j in (0..=i).rev() {
+                if dp[j] >= s[0] {
+                    dp[j + 1] = std::cmp::max(dp[j + 1], dp[j] + s[1]);
                 }
             }
-            heap.push(s[1]);
         }
-        k
+        for (i, &e) in dp.iter().enumerate() {
+            if e >= target {
+                return i as i32;    
+            }
+        }
+        -1
     }
 }
