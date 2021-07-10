289183d4774fea3590e485d5b747851e34fb224e
Quentin Fortier <qpfortier@gmail.com>
2021-07-10 18:18:53 +0200

    Use Dijkstra

--- graph/minimum-cost-to-reach-destination-in-time.py
+++ graph/minimum-cost-to-reach-destination-in-time.py
@@ -1,19 +1,33 @@
-import numpy as np
+# Dijkstra with (cost, time) or (time, cost)
+# dp (floyd-warshall like) works too
 
 class Solution:
     def minCost(self, maxTime: int, edges: List[List[int]], passingFees: List[int]) -> int:
         n = len(passingFees)
-        M = [[] for i in range(n)]
+        
+        g = [[] for i in range(n)]
         for e in edges:
-            M[e[0]].append((e[1], e[2]))
-            M[e[1]].append((e[0], e[2]))
-        dp = np.full((maxTime + 1, n), float('inf'))  # dp[t, v] is the minimum cost of a path from 0 to v with time <= t
-        dp[0, 0] = passingFees[0]
-        reach = False
-        for t in range(1, maxTime + 1):
-            for u in range(n):
-                for v, time in M[u]:
-                    if time <= t:
-                        dp[t, u] = min(dp[t, u], dp[t - time, v] + passingFees[u])
-        r = min(dp[:, n-1])
-        return -1 if r == float('inf') else int(r)
+            g[e[0]].append((e[1], e[2]))
+            g[e[1]].append((e[0], e[2]))
+            
+        times = {}
+        
+        pq = [(passingFees[0],0,0)]
+        
+        while pq:
+            cost, node, time = heapq.heappop(pq)
+            
+            if time > maxTime:
+                continue
+            
+            if node == n-1:
+                return cost
+            
+            if node not in times or times[node] > time:
+                times[node] = time
+                for nbor, trip in g[node]:
+                    heapq.heappush(pq, (passingFees[nbor]+cost, nbor, time+trip))
+            
+            
+    
+        return -1
